b75aa9833c09ae14430356dbcf287dc53c8361fd
JeanPiton <jean.orpiton@gmail.com>
2023-07-26 16:13:44 -0300

    Feature: post games

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -8,3 +8,16 @@ export async function getGames(req,res){
         res.status(501).send(err.message)
     }
 }
+
+export async function postGames(req,res){
+    const {name,image,stockTotal,pricePerDay} = req.body
+
+    try {
+        const used = await db.query("SELECT * FROM games WHERE name = $1 LIMIT 1;",[name])
+        if(used.rows.length>0) return res.sendStatus(409)
+        await db.query('INSERT INTO games (name,image,"stockTotal","pricePerDay") VALUES($1,$2,$3,$4);',[name,image,stockTotal,pricePerDay])
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(501).send(err.message)
+    }
+}
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { validateSchema } from "../middleware/validateSchema.js";
-import { getGames } from "../controllers/games.controllers.js";
+import { getGames, postGames } from "../controllers/games.controllers.js";
+import { schemaGame } from "../schemas/games.schemas.js";
 
 const gamesRouter = Router()
 
 gamesRouter.get("/games", getGames)
+gamesRouter.post("/games",validateSchema(schemaGame),postGames)
 
 export default gamesRouter
