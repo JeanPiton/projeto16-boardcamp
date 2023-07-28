190673bc32527959d074e0771aad45531c185d86
JeanPiton <jean.orpiton@gmail.com>
2023-07-28 02:13:47 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express, { json } from "express"
+import dotenv from "dotenv"
 import cors from "cors"
 import router from "./routes/index.routes.js"
+dotenv.config()
 
 const server = express()
 
