81342a4911df58062056cc1776dfe84c81fbfea0
JeanPiton <jean.orpiton@gmail.com>
2023-07-28 00:39:55 -0300

    Feature: post rentals

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -13,3 +13,21 @@ export async function getRentals(req,res){
         res.status(501).send(err.message)
     }
 }
+
+export async function postRentals(req,res){
+    const {customerId,gameId,daysRented} = req.body
+
+    try {
+        const customer = await db.query(`SELECT * FROM customers WHERE id=$1`,[customerId])
+        const game = await db.query(`SELECT * FROM games WHERE id=$1`,[gameId])
+        const rents = await db.query(`SELECT * FROM rentals WHERE "gameId"=$1`,[gameId])
+        if(customer.rows.length==0||game.rows.length==0||rents.rows.length>=game.rows[0].stockTotal) return res.sendStatus(400)
+
+        await db.query(`INSERT INTO rentals ("customerId","gameId","rentDate","daysRented","originalPrice")
+        VALUES ($1,$2,CURRENT_DATE,$3,$4)`,
+        [customerId,gameId,daysRented,game.rows[0].stockTotal*daysRented])
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(501).send(err.message)
+    }
+}
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { validateParamSchema, validateSchema } from "../middleware/validateSchema.js";
-import { getRentals } from "../controllers/rentals.controllers.js";
+import { getRentals, postRentals } from "../controllers/rentals.controllers.js";
+import { rentalSchema } from "../schemas/rentals.schemas.js";
 
 const rentalRouter = Router()
 
 rentalRouter.get("/rentals",getRentals)
+rentalRouter.post("/rentals",validateSchema(rentalSchema),postRentals)
 
 export default rentalRouter
