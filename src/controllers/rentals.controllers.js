54506c40d1a52327be33a6454b16d375cff1bf7a
JeanPiton <jean.orpiton@gmail.com>
2023-07-28 16:03:08 -0300

    Fix: bonus rental query string

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -9,9 +9,9 @@ export async function getRentals(req,res){
         const {rows} = await db.query(`SELECT rentals.*,customers.name AS "customerName",games.name AS "gameName" 
         FROM rentals JOIN customers ON rentals."customerId"=customers.id 
         JOIN games ON rentals."gameId"=games.id
-        WHERE CAST("customerId" AS TEXT) ILIKE '$1'
-        OR CAST("gameId" AS TEXT) ILIKE '$2'
-        LIMIT $3 OFFSET $4`,[customerId,gameId,limit,offset])
+        WHERE CAST("customerId" AS TEXT) ILIKE $1
+        OR CAST("gameId" AS TEXT) ILIKE $2
+        LIMIT $3 OFFSET $4`,[customerId+"%",gameId+"%",limit,offset])
         const list = rows.map(e=>{
             let {customerName,gameName, ...rest} = e
             return {...rest,customer:{id:e.customerId,name:customerName},game:{id:e.gameId,name:gameName}}})
