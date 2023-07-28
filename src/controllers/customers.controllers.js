e5cb1f66905128744b43534612f2d25750121d07
JeanPiton <jean.orpiton@gmail.com>
2023-07-28 13:09:54 -0300

    Fix: customer birthday display

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -2,7 +2,7 @@ import { db } from "../database/database.connection.js";
 
 export async function getCustomers(req,res){
     try {
-        const list = await db.query('SELECT * FROM customers')
+        const list = await db.query(`SELECT *,TO_CHAR(birthday,'YYYY-MM-DD') AS birthday FROM customers`)
         res.status(200).send(list.rows)
     } catch (err) {
         res.status(501).send(err.message)
@@ -13,7 +13,7 @@ export async function getById(req,res){
     const {id} = req.params
     
     try {
-        const customer = await db.query('SELECT * FROM customers WHERE id=$1',[id])
+        const customer = await db.query(`SELECT *,TO_CHAR(birthday,'YYYY-MM-DD') AS birthday FROM customers WHERE id=$1`,[id])
         if(customer.rows.length==0) return res.sendStatus(404)
         res.status(200).send(customer.rows[0])
     } catch (err) {
@@ -43,7 +43,7 @@ export async function putCustomer(req,res){
         const exists = await db.query('SELECT * FROM customers WHERE cpf=$1 AND id!=$2',[cpf,id])
         if(exists.rows.length!=0) return res.sendStatus(409)
         await db.query('UPDATE customers SET name=$1,phone=$2,cpf=$3,birthday=$4 WHERE id=$5',[name,phone,cpf,birthday,id])
-        res.sendStatus(201)
+        res.sendStatus(200)
     } catch (err) {
         if(err.routine == 'DateTimeParseError') return res.sendStatus(400)
         res.status(501).send(err.message)
--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,4 +1,6 @@
 import pg from "pg"
+import dotenv from "dotenv"
+dotenv.config()
 
 const { Pool } = pg
 
@@ -6,6 +8,6 @@ const configDatabase = {
     connectionString: process.env.DATABASE_URL,
 }
 
-if(process.env.NODE_ENV === "production") configDatabase.ssl = true
+//if(process.env.NODE_ENV === "production") configDatabase.ssl = true
 
 export const db = new Pool(configDatabase)
